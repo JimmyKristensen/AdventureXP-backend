86696ad2ef09df5d815e6e1b362170935021dad2
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-10-04 12:30:19 +0200

    Activity Entity Corrected

--- src/main/java/com/example/backend/Acticity/Model/Activity.java
+++ src/main/java/com/example/backend/Acticity/Model/Activity.java
@@ -22,34 +22,47 @@ public class Activity {
     private String type;
 
     @Column(name="Duration")
-    private String duration;
+    private Integer duration;
+
+    @Column(name="Maintenance")
+    private Integer maintenance;
+
+    @Column(name="Max Amount Of People")
+    private Integer maxAmountOfPeople;
+
+    @Column(name="Description")
+    private String description;
 
     @Column(name="Picture")
     private String picture; //Path or URL.
 
-    @Column(name="Min Length")
-    private String minLength;
+    @Column(name="Price")
+    private Integer price;
 
     @Column(name="Min age")
     private String minAge;
 
-    @Column(name="Max Pers")
-    private Integer maxPers;
+    @Column(name="Min Height")
+    private String minHeight;
 
-    @Column(name="Price")
-    private Integer price;
+    @Column(name="Restrictions")
+    private String restrictions;
 
     //Relation til Reservation
 
-    public Activity(Integer activityId, String type, String duration, String picture, String minLength, String minAge, Integer maxPers, Integer price) {
+
+    public Activity(Integer activityId, String type, Integer duration, Integer maintenance, Integer maxAmountOfPeople, String description, String picture, Integer price, String minAge, String minHeight, String restrictions) {
         this.activityId = activityId;
         this.type = type;
         this.duration = duration;
+        this.maintenance = maintenance;
+        this.maxAmountOfPeople = maxAmountOfPeople;
+        this.description = description;
         this.picture = picture;
-        this.minLength = minLength;
-        this.minAge = minAge;
-        this.maxPers = maxPers;
         this.price = price;
+        this.minAge = minAge;
+        this.minHeight = minHeight;
+        this.restrictions = restrictions;
     }
 
     @Override
@@ -57,12 +70,15 @@ public class Activity {
         return "Activity{" +
                 "activityId=" + activityId +
                 ", type='" + type + '\'' +
-                ", duration='" + duration + '\'' +
+                ", duration=" + duration +
+                ", maintenance=" + maintenance +
+                ", maxAmountOfPeople=" + maxAmountOfPeople +
+                ", description='" + description + '\'' +
                 ", picture='" + picture + '\'' +
-                ", minLength='" + minLength + '\'' +
-                ", minAge='" + minAge + '\'' +
-                ", maxPers=" + maxPers +
                 ", price=" + price +
+                ", minAge='" + minAge + '\'' +
+                ", minHeight='" + minHeight + '\'' +
+                ", restrictions='" + restrictions + '\'' +
                 '}';
     }
 }
