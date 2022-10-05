77cacaa18951850bfcf82b1fe63fe3d24719a53c
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-10-05 11:50:48 +0200

    Nicolas, Test data

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,7 +1,18 @@
 package com.example.backend;
 
+import com.example.backend.Acticity.Model.Activity;
+import com.example.backend.Acticity.Repository.ActivityRepository;
+import com.example.backend.Customer.Model.Customer;
+import com.example.backend.Customer.Repository.CustomerRepository;
+import com.example.backend.Reservation.Model.Reservation;
+import com.example.backend.Reservation.Repository.ReservationRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +21,25 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner importData(CustomerRepository customerRepository,
+                                        ActivityRepository activityRepository,
+                                        ReservationRepository reservationRepository
+
+    ){
+        return (args) ->{
+
+            final List<Customer> customers = new ArrayList<>();
+            customers.add(new Customer("Nicolas",53341573,"nicolas.mousten@gmail.com"));
+            customerRepository.saveAll(customers);
+
+            final List<Activity> activities = new ArrayList<>();
+            activities.add(new Activity("Go Cart",25,5,6,"BLABLABLA","PathToPic",499, "11 years", "150cm", "No prior injuries"));
+            activityRepository.saveAll(activities);
+
+            final List<Reservation> reservations = new ArrayList<>();
+            reservations.add(new Reservation(1,1,6));
+            reservationRepository.saveAll(reservations);
+        };
+    }
 }
