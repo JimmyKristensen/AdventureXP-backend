5b824042b6c1474b9f2dc2aee41caba82aa726a4
JimmyKristensen <siswanto@live.dk>
2022-10-13 12:45:51 +0200

    added @Service

--- src/main/java/com/example/backend/TimeTableSlot/Service/ReservationValidator.java
+++ src/main/java/com/example/backend/TimeTableSlot/Service/ReservationValidator.java
@@ -1,5 +1,7 @@
 package com.example.backend.TimeTableSlot.Service;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -7,6 +9,7 @@ import java.util.Locale;
 
 //SAMUEL
 
+@Service
 public class ReservationValidator {
     public ReservationValidator() {
         //Get Reservation               https://www.javaguides.net/2018/12/different-ways-to-retrieve-jpa-entity-objects-from-database.html
